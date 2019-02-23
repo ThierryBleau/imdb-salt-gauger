1c72b3ce6ba9cd3167be3a9dcf6ceab926b4d2ae
BotondMaros <botond.maros@mail.mcgill.ca>
2019-02-22 19:03:55 -0500

    final

--- sgd.py
+++ sgd.py
@@ -6,8 +6,9 @@ from sklearn.preprocessing import Normalizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 from sklearn import metrics
+import pandas
 
-X_train, X_test, y_train, y_test = data_scan.main_test()
+X_train, y_train, X_test, y_test = data_scan.main()
 
 vader_train = vader_score.vader(X_train)
 vader_test = vader_score.vader(X_test)
@@ -30,15 +31,15 @@ X_test_normalized = normalizer_tranformer.transform(X_test_tfidf)
 sgd = SGDClassifier(loss='log').fit(X_train_normalized, y_train)
 y_pred_normal = sgd.predict(X_test_normalized)
 y_pred_proba = sgd.predict_proba(X_test_normalized)
-print(y_pred_proba.shape())
-
+print(y_pred_proba.shape)
+print(y_pred_proba[0])
+y_pred_vader = []
 for i in range(0,len(y_pred_proba)):
-    y_pred_vader = []
-    if ((y_pred_proba[0][i] + vader_test[i]) / 2) > 0.5:
+    if ((y_pred_proba[i][1] + vader_test[i]) / 2) > 0.5:
         y_pred_vader.append(1)
     else:
         y_pred_vader.append(0)
-
+""" print(y_pred_vader)
 counter=0
 for i in range(0,len(y_pred_vader)):
     if y_test[i]!=y_pred_vader[i]:
@@ -49,5 +50,8 @@ print(y_test[0:9])
 print("==")
 print(y_pred_vader[0:9])
 print(avg)
-print(metrics.classification_report(y_test, y_pred_normal))
+print(metrics.classification_report(y_test, y_pred_normal)) """
+
+df = pandas.DataFrame(data={"ID": y_test, "Category": y_pred_vader})
+df.to_csv("./file.csv", sep=',',index=False)
 
